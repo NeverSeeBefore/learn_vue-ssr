50863a5bd3dd89bde3b434e4bda0224004c12d19
chenxiaoxu <v_chenxiaoxu01@baidu.com>
2020-10-10 17:17:20 +0800

    创建了app.vue,忘了app.js要做什么

--- server/index.js
+++ server/index.js
@@ -1,11 +1,14 @@
 const path = require('path');
+const fs = require('fs');
 const express = require('express');
 const Vue = require('vue');
 const vueServerRenderer = require('vue-server-renderer');
 
 
 const server = express();
-const render = vueServerRenderer.createRenderer();
+const render = vueServerRenderer.createRenderer({
+    template: fs.readFileSync(path.join(__dirname, '../index.template.html'), 'utf-8')
+});
 
 server.use(express.static(path.join(__dirname, '../assets/home')))
 
@@ -18,7 +21,11 @@ server.use('/ssr', (req, res) => {
         }
     });
 
-    render.renderToString(app, (err, html) => {
+    render.renderToString(app, {
+        description: 'description',
+        keyword: 'a, b, c, d, e, f, g',
+        data: '<meta name="test" content="test content">'
+    }, (err, html) => {
         if(err){
             throw err;
         }

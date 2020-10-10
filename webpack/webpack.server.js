07ef25352ca8e8e506bb1d16a0953b408a071a0a
chenxiaoxu <v_chenxiaoxu01@baidu.com>
2020-10-10 17:33:24 +0800

    server-entry.js 做了什么

--- webpack/webpack.server.js
+++ webpack/webpack.server.js
@@ -1,5 +1,5 @@
 const path = require('path');
-const VueLoaderPlugin = require('vue-loader/dist/plugin');
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
 module.exports = {
     mode: 'development',
     target: 'node',
